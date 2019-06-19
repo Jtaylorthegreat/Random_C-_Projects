650b40dce1df5360f1ffd2c0ffd800f1c1bcb4de
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2019-06-19 15:44:35 -0500

    Create heartbeat_client.cpp

---
+++ heartbeat_client.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+	/*check if port number is provided
+	if (argc != 2) {
+        cerr << "no port number provided after command" << endl;
+        return -1;
+    }*/
+	
+	int csocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (csocket == -1) {
+		cerr << "Can't create socket! Exiting" << endl;
+		return -1;
+	}
+	
+	string agentconfig = ".agentconfig";
+	ifstream fin(agentconfig.c_str());
+		if(!fin) {
+			cout << "Agent config file not found" << ".\n";
+			cout << "Initialzing setup" << ".\n";
+			string interactiveinput;
+			ofstream settingfile(".agentconfig");
+			cout << "please enter the heartbeat server IP address ";
+			getline (cin, interactiveinput);
+			settingfile << interactiveinput << endl;
+			cout << "please enter the heartbeat server port ";
+			getline (cin, interactiveinput);
+			settingfile << interactiveinput << endl;
+			cout << "Please enter unique 8 character key ";
+			getline (cin, interactiveinput);
+			settingfile << interactiveinput << endl;
+			settingfile.close();
+		}
+	
+	
+	while (true) {	
+	ifstream infile;
+	string IP, PORT, KEY;
+		infile.open(".agentconfig");
+		getline(infile, IP, '\n');
+		getline(infile, PORT, '\n');
+		getline(infile, KEY, '\n');
+		infile.close();
+		
+		int servPORT = stoi(PORT);
+		sockaddr_in tech;
+		tech.sin_family = AF_INET;
+		tech.sin_port = htons(servPORT);
+		inet_pton(AF_INET, IP.c_str(), &tech.sin_addr);
+		
+	int conreq = connect(csocket, (sockaddr*)&tech, sizeof(tech));
+	if (conreq < 0 ){
+		cerr << "error connecting" << endl;
+		return -1;
+	}
+	char buf [4096];
+	int sendmessage = send(csocket, KEY.c_str(), KEY.size() +1, 0);
+	if (sendmessage < 0){
+		cerr << "Error sending" << endl;
+		return -1;
+	}
+    sleep(30);
+	}
+	
+	
+
+}
