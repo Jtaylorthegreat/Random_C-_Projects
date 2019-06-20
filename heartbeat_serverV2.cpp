50dbe5e7922a2dcfa7c9801891a957b64d7348e3
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2019-06-20 14:36:34 -0500

    Create heartbeat_serverV2.cpp

---
+++ heartbeat_serverV2.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+#include <stdlib.h>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+ 
+  
+ 
+void hblisten (int clientSocket){
+    char buf[4096];
+    memset(buf, 0, 4096);
+    int clienthblisten = recv(clientSocket, buf, 4096, 0);
+    int clientok;
+    time_t now = time(0);
+    char* servtime = ctime(&now);
+    if (clienthblisten == -1) {
+        cerr << "Error receiving client HB" << endl;
+        exit(1);
+    }
+    if (clienthblisten == 0) {
+        cout << "Client disconnected" << endl;
+        exit(1);
+    }
+    if (clienthblisten >1){
+        clientok == 1;
+    }
+ cout << "client key " << string(buf, 0, clienthblisten) << " successful heartbeat " << servtime << endl;       
+}
+ 
+int main()
+{
+    
+    string serverconfig = ".serverconfig";
+    ifstream fin(serverconfig.c_str());
+        if(!fin) {
+                cout << "Server config file not found" << ".\n";
+                cout << "Initializing setup" << ".\n";
+                string interactiveinput;
+                ofstream settingfile(".serverconfig");
+                cout << "Please Enter listening port ";
+                getline (cin, interactiveinput);
+                settingfile << interactiveinput << endl;
+                cout << "Please enter pre-shared key ";
+                getline (cin, interactiveinput);
+                settingfile << interactiveinput << endl;
+                settingfile << "0" << endl;
+                settingfile.close();
+                cout << "Configuration file .serverconfig created" << ".\n";
+                cout << "Default Log level set to 0" << ".\n";
+        }
+       
+   //pull configs from server config file   
+   ifstream infile;
+     string SPORT;
+     //string SPORT, PSK, LL;
+        infile.open(".serverconfig");
+        getline(infile, SPORT, '\n');
+        //getline(infile, PSK, '\n');
+        //getline(infile, LL, '\n');
+        infile.close();
+        
+    
+   //Assign conf values from config file
+    int PORT;
+    //int PORT, SECRET_KEY, LOG_LEVEL;
+    PORT = stoi(SPORT);
+    //SECRET_KEY = stoi(PSK);
+    //LOG_LEVEL = stoi(LL);
+    
+    
+        
+    int mysocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mysocket == -1) {
+        cerr << "Can't create a socket! Quitting" << endl;
+        return -1;
+    }
+    // Bind the ip address and port to a socket
+    sockaddr_in tech;
+    tech.sin_family = AF_INET;
+    tech.sin_port = htons(PORT);
+    tech.sin_addr.s_addr = INADDR_ANY;
+    bind(mysocket, (sockaddr*)&tech, sizeof(tech));
+    
+    //added keep alive to socket:
+    int optval;
+    socklen_t optlen = sizeof(optval);
+    setsockopt(mysocket, SOL_SOCKET, SO_KEEPALIVE, &optval, optlen); 
+    
+    listen(mysocket, SOMAXCONN);
+   
+ 
+    // Wait for a connection
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    int clientsocket, pid;
+    
+    
+ while (true) {
+    clientsocket = accept(mysocket, (sockaddr*)&client, &clientSize);
+    
+        if (clientsocket < 0) {
+            cerr << "Error on accept" << endl;
+            return -1;
+        }
+        
+        pid = fork ();
+         
+        if (pid <0) {
+             cerr << "error forking" << endl;
+             return -1;
+        }
+        
+        if (pid == 0) {
+            close(mysocket);
+            hblisten(clientsocket);
+            exit (0);
+        }
+        else {
+            close(clientsocket);
+        }
+ 
+ }
+ 
+ return 0;
+ 
+}
