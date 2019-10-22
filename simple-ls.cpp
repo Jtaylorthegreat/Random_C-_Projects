8472234afdcf50def7d428ca41d67a6b1c42de26
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2019-10-22 17:05:09 -0500

    Update simple-ls.cpp

--- simple-ls.cpp
+++ simple-ls.cpp
@@ -4,14 +4,44 @@
 #include <iostream>
 #include <vector>
 #include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
+
 typedef std::vector<std::string> stringvec;
  
-void read_directory(const std::string& dirname, stringvec& vec)
+void read_dirs_in_directory(const std::string& dirname, stringvec& vec)
 {
-    DIR* direc = opendir(dirname.c_str());
+	unsigned char isFolder =0x4;
+	DIR* direc = opendir(dirname.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(direc)) != NULL) {
+        //vec.push_back(dp->d_name);
+        //if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0 && dp->d_type == isFolder)
+        	vec.push_back(dp->d_name);
+    }
+    closedir(direc);
+}
+
+void read_files_in_directory(const std::string& dirname, stringvec& vec)
+{
+	unsigned char isFile =0x8;
+	DIR* direc = opendir(dirname.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(direc)) != NULL) {
+        //vec.push_back(dp->d_name);
+        //if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0 && dp->d_type == isFile)
+        	vec.push_back(dp->d_name);
+    }
+    closedir(direc);
+}
+
+void read_all_in_directory(const std::string& dirname, stringvec& vec)
+{
+	DIR* direc = opendir(dirname.c_str());
     struct dirent * dp;
     while ((dp = readdir(direc)) != NULL) {
         //vec.push_back(dp->d_name);
@@ -21,19 +51,82 @@ void read_directory(const std::string& dirname, stringvec& vec)
     closedir(direc);
 }
 
+void print_help(){
+		printf("\n");
+        printf("                             Simple Ls                                      \n");
+        printf("\n");
+        printf("                   Usage: ./simple-ls [option] location                     \n");
+        printf("options:\n");
+        printf("\n");
+        printf(" -h                                                           show this help\n");
+        printf(" -f                                                   show files in location\n");
+        printf(" -d                                             show directories in location\n");
+        printf(" -a                        show all (both files and directories) in location\n");
+        printf("\n");
+}
+
 int main(int argc, char *argv[]) {
-	if (argc < 2){
-		cout << "Usage: ./simple-ls directory" << endl;
+	if (argc < 3){
+		print_help();
 		return -1;
 	}
-	vector<string> vect;
-	int numoffiles;
-	const string passdir(argv[1]);
-	read_directory(passdir.c_str(), vect);
-	numoffiles = vect.size();
-	cout << "num of items in " << passdir << ": " << numoffiles << endl;
-	for (int i = 0; i < numoffiles; i++){
-		cout << vect[i] << endl;
+	if (argc > 3){
+		print_help();
+		return -1;
 	}
+	string suppliedoption(argv[1]);
+    if (suppliedoption == "-h") {
+    	print_help();
+    	return 0;
+    }
+    if (suppliedoption == "-f") {
+    	vector<string> vect;
+		int numoffiles =0;
+		const string passdir(argv[2]);
+		read_files_in_directory(passdir.c_str(), vect);
+		numoffiles = vect.size();
+		sort(vect.begin(), vect.end());
+		cout << "num of files in " << passdir << ": " << numoffiles << endl;
+		if (numoffiles > 0 ) {
+			for (int i = 0; i < numoffiles; i++){
+				cout << vect[i] << endl;
+			}
+		}
+		return 0;
+    }
+    if (suppliedoption == "-d") {
+    	vector<string> vect;
+		int numoffiles =0;
+		const string passdir(argv[2]);
+		read_dirs_in_directory(passdir.c_str(), vect);
+		numoffiles = vect.size();
+		sort(vect.begin(), vect.end());
+		cout << "num of directories in " << passdir << ": " << numoffiles << endl;
+		if (numoffiles > 0 ) {
+			for (int i = 0; i < numoffiles; i++){
+				cout << vect[i] << endl;
+			}
+		}
+		return 0;
+    }
+    if (suppliedoption == "-a") {
+    	vector<string> vect;
+		int numoffiles =0;
+		const string passdir(argv[2]);
+		read_all_in_directory(passdir.c_str(), vect);
+		numoffiles = vect.size();
+		sort(vect.begin(), vect.end());
+		cout << "num of items in " << passdir << ": " << numoffiles << endl;
+		if (numoffiles > 0 ) {
+			for (int i = 0; i < numoffiles; i++){
+				cout << vect[i] << endl;
+			}
+		}
+		return 0;
+    }
+
+
+
+	
 	return 0;
 }
