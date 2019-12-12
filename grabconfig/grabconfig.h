e03729dc6f7f5689c49ea84721f86fb2cfd41428
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2019-12-12 13:33:22 -0600

    Create grabconfig.h

---
+++ grabconfig/grabconfig.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+struct configfile {
+    string loadedstructvalue1;
+    string loadedstructvalue2;
+    string loadedstructvalue3;
+    string loadedstructvalue4;
+  };
+
+
+extern string ASSIGNEDVALUE1, ASSIGNEDVALUE2, ASSIGNEDVALUE3, ASSIGNEDVALUE4;
